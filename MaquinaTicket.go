11f9ff8e6d5c296faa4ec5b6d321d52c49e1e291
junior2pp <junior2pp@hotmail.com>
2017-06-06 00:55:57 -0500

    rama

--- MaquinaTicket.go
+++ MaquinaTicket.go
@@ -1,5 +1,7 @@
 package maquina
 
+import "fmt"
+
 type Ticket struct {
 	precio   int
 	balance  int
@@ -9,6 +11,9 @@ type Ticket struct {
 
 func (t *Ticket) Init(ticketCost int) {
 	t.precio = ticketCost
+	t.balance = 0
+	t.total = 0
+	t.contador = 0
 }
 func (t *Ticket) GetPrecio() int {
 	return t.precio
@@ -17,13 +22,13 @@ func (t *Ticket) GetBalance() int {
 	return t.balance
 }
 func (t *Ticket) SetBalance(balanc int) {
-	t.balance = balanc
+	if balanc > 0 {
+		t.balance += balanc
+	}
+
 }
 func (t *Ticket) GetContador() int {
 	return t.contador
-}
-func (t *Ticket) IncrementarContador() {
-	t.contador++
 }
 func (t *Ticket) GetTotal() int {
 	return t.total
@@ -32,10 +37,21 @@ func (t *Ticket) SetTotal(totaly int) {
 	t.total = totaly
 }
 func (t *Ticket) VaciarMaquina() int {
-	TempTotal := t.total
+	tempTotal := t.total
 	t.total = 0
-	return TempTotal
+	t.contador = 0
+	return tempTotal
 }
-func (t *Ticket) ImprimirTicket() (int, int) {
-	return t.contador, t.precio
+func (t *Ticket) ImprimirTicket() {
+	if t.balance >= t.precio {
+		t.contador++
+		t.balance = t.balance - t.precio
+		t.total = t.total + t.precio
+		fmt.Println("----------------------")
+		fmt.Println("TICKET #", t.contador)
+		fmt.Println("PRECIO", t.precio)
+		fmt.Println("----------------------")
+	} else {
+		fmt.Printf("Necesita %v centavos mas", (t.precio - t.balance))
+	}
 }
